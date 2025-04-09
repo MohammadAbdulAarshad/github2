c09f808290a8fd667ea319e81ed09359ce0f0eb7
Mohammad Abdul Aarshad <abdulaarshadmohammad@gmail.com>
2025-04-10 02:02:52 +0530

    first commit

---
+++ arsh.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class arsh {
+    public static void main(String[] args) {
+
+       System.out.println("hello world ytyfrtyru ");
+    }
+}
+/*git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MohammadAbdulAarshad/int1.git
+git push -u origin main
+*/
+
